13b8a0ff678ffbaeb0248c16eaa9d5b241a31a5d
NeginOlfat <neginolfat@yahoo.com>
2022-07-25 13:18:09 +0430

    Styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+            .then(response => {
+                return response.json()
+            })
+            .then(users => {
+                this.setState({ robots: users })
+            })
     }
 
     onSearchChange = (event) => {
@@ -20,13 +31,20 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className='tc' >
-                <h2>RoboFriends</h2>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        )
+
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc' >
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            )
+        }
     }
 }
 
